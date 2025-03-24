7358669c86201b18d319678007e13f1ed9438716
mar <martorrijos@protonmail.com>
2025-03-24 16:32:05 +0100

    Lvl 1 ejercici 2 with testing

---
+++ src/test/java/org/example/Level_1/ejercici_2/DniCalculatorTest.java
@@ -0,0 +1,17 @@
+package org.example.Level_1.ejercici_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DniCalculatorTest {
+
+    @Test
+    void CalculateLetterTest() {
+        assertEquals('F', dniCalculator.calculateLetter(98432210));
+        assertEquals('J', dniCalculator.calculateLetter(22520877));
+        assertEquals('F', dniCalculator.calculateLetter(52436741));
+        assertEquals('B', dniCalculator.calculateLetter(14569752));
+    }
+
+}
