39bcfc66272fde9a5473ca712e3c486fdea66090
mar <martorrijos@protonmail.com>
2025-03-25 17:55:55 +0100

    Level 2: Some corrections + all the remaining exercises

---
+++ src/main/java/org/Level_2/ejercici_6/arrayException.java
@@ -0,0 +1,13 @@
+package org.Level_2.ejercici_6;
+
+public class arrayException {
+
+    public static String[] myList = new String[5];
+
+    public static void setUp(){
+        myList[0] = "Café";
+        myList[1] = "Arroz";
+        myList[2] = "Tofu";
+    }
+
+}
